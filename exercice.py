1332db6c39750e2eaad524af74cbbcfc46639132
hugomatijascic <hugo.matijascic@polymtl.ca>
2020-09-09 20:24:27 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -9,7 +9,7 @@ def square_root(number: int) -> float:
 
 def square(number: int) -> int:
     # TODO completer la fonction
-   return math.sqrt(number)
+    return math.sqrt(number)
 
 def main() -> None:
     for i in range(25):
