acf095e97ed5159c33ed59c3bca0a1ba746bbda3
hugomatijascic <hugo.matijascic@polymtl.ca>
2020-09-09 20:18:46 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -12,7 +12,6 @@ def square(number: int) -> int:
     # TODO completer la fonction
     return math.pow(number, 2)
 
-
 def main() -> None:
     for i in range(25):
         print(f"Square root: {square_root(i)}, square: {square(i)}")
